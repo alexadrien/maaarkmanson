8ec834d815cdb9d9ba00f8f492953c8589f0145e
AAA <alexadriena@sipios.com>
2023-06-12 12:21:19 +0200

    Add Sentry

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,23 @@ import "@fontsource/montserrat/500.css";
 import "@fontsource/montserrat/700.css";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://851ae6be07ab44cfa19d751b22a87130@o4505346053308416.ingest.sentry.io/4505346054356992",
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ["localhost", /^https:\/\/maaarkmanson\.net\//],
+    }),
+    new Sentry.Replay(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
