df6346b95289dcb9aa5c619e4a552b462e81e788
AAA <alexadriena@sipios.com>
2023-06-15 21:15:24 +0200

    Change prompt

--- src/useCompletions.ts
+++ src/useCompletions.ts
@@ -29,7 +29,7 @@ export const useCompletions = () => {
         End every message with a self-reflecting question to the user.
         Use patient's first name everytime it is possible.
         Start by asking for the user's first name.
-        When using a quote from Mark Manson, answer like you are Mark Manson`
+        When using a quote from Mark Manson, answer using 'I' statements`
       ),
       ...history.slice(0, history.length - 1).map(constructMessage),
       new SystemChatMessage(`Use the quote from Mark Manson delimited by triple quotes as an inspiration for your next message if relevant to the last user message:
