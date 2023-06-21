89bb9a2f9beb421d9375f883fb0e18be72972606
AAA <alexadriena@sipios.com>
2023-06-21 17:18:58 +0200

    Make model configuration explicit so that I'm sure it's the same as the playground

--- src/hooks/useNextMessage.ts
+++ src/hooks/useNextMessage.ts
@@ -3,6 +3,7 @@ import { Configuration, OpenAIApi } from "openai";
 import { useRecoilValue } from "recoil";
 import { openAiApiKeyAtom } from "../atoms";
 import { NO_OPENAI_KEY, UNABLE_TO_PRODUCE_MESSAGE } from "../errors";
+import { CreateChatCompletionRequest } from "openai/api";
 
 export const useNextMessage = () => {
   const apiKey = useRecoilValue(openAiApiKeyAtom);
@@ -14,10 +15,13 @@ export const useNextMessage = () => {
     const params = { apiKey };
     const configuration = new Configuration(params);
     const openai = new OpenAIApi(configuration);
-    const completionRequest = {
+    const completionRequest: CreateChatCompletionRequest = {
       model: "gpt-3.5-turbo",
       messages: history,
       temperature,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
     };
     const chatCompletion = await openai.createChatCompletion(completionRequest);
     const newMessage = chatCompletion.data.choices[0].message;
